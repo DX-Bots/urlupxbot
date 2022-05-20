49a79df47d9253958f9c1bea5e1bf0595c96c0d3
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-05-20 18:07:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/Tellybots/Uploader-Bot-V2 /Uploader-Bot-V2
 cd /Uploader-Bot-V2
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
-python3 bot.py
+python3 -m bot.py
